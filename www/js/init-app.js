a740afdc215a4ac6b0ce7f9647864368e421a0d4
Tom Main <tommain1498@gmail.com>
2018-03-01 22:10:58 +0000

    second

--- www/js/init-app.js
+++ www/js/init-app.js
@@ -32,109 +32,73 @@ app.consoleLog = function() {           // only emits console.log messages if ap
 
 
 
-// App init point (runs on custom app.Ready event from init-dev.js).
-// Runs after underlying device native code and webview/browser is ready.
-// Where you should "kick off" your application by initializing app events, etc.
-
-// NOTE: Customize this function to initialize your application, as needed.
-
-app.initEvents = function() {
-    "use strict" ;
-    var fName = "app.initEvents():" ;
-    app.consoleLog(fName, "entry") ;
-
-    // NOTE: initialize your third-party libraries and event handlers
-
-    // initThirdPartyLibraryNumberOne() ;
-    // initThirdPartyLibraryNumberTwo() ;
-    // initThirdPartyLibraryNumberEtc() ;
-
-    // NOTE: initialize your application code
-
-    // initMyAppCodeNumberOne() ;
-    // initMyAppCodeNumberTwo() ;
-    // initMyAppCodeNumberEtc() ;
-
-    // NOTE: initialize your app event handlers, see app.js for a simple event handler example
-
-    // TODO: configure following to work with both touch and click events (mouse + touch)
-    // see http://msopentech.com/blog/2013/09/16/add-pinch-pointer-events-apache-cordova-phonegap-app/
-
-    var el, evt ;
-
-    if( navigator.msPointerEnabled || !('ontouchend' in window))    // if on Win 8 machine or no touch
-        evt = "click" ;                                             // let touch become a click event
-    else                                                            // else, assume touch events available
-        evt = "touchend" ;                                          // not optimum, but works
-
-    el = document.getElementById("id_btnHello") ;
-    el.addEventListener(evt, myEventHandler, false) ;
-
-    // NOTE: ...you can put other miscellaneous init stuff in this function...
-    // NOTE: ...and add whatever else you want to do now that the app has started...
-    // NOTE: ...or create your own init handlers outside of this file that trigger off the "app.Ready" event...
-
-    app.initDebug() ;           // just for debug, not required; keep it if you want it or get rid of it
-    app.hideSplashScreen() ;    // after init is good time to remove splash screen; using a splash screen is optional
-
-    // app initialization is done
-    // app event handlers are ready
-    // exit to idle state and wait for app events...
-
-    app.consoleLog(fName, "exit") ;
-} ;
-document.addEventListener("app.Ready", app.initEvents, false) ;
 
 
 
-// Just a bunch of useful debug console.log() messages.
-// Runs after underlying device native code and webview/browser is ready.
-// The following is just for debug, not required; keep it if you want or get rid of it.
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+console.log("ready boi");
 
-app.initDebug = function() {
-    "use strict" ;
-    var fName = "app.initDebug():" ;
-    app.consoleLog(fName, "entry") ;
 
-    if( window.device && device.cordova ) {                     // old Cordova 2.x version detection
-        app.consoleLog("device.version: " + device.cordova) ;   // print the cordova version string...
-        app.consoleLog("device.model: " + device.model) ;
-        app.consoleLog("device.platform: " + device.platform) ;
-        app.consoleLog("device.version: " + device.version) ;
+    
+    
+    
+
+    
+    
+    
+}
+
+
+function cameraTakePicture() { 
+    console.log("clicko");
+
+    function openCamera(selection) {
+
+    var srcType = Camera.PictureSourceType.CAMERA;
+    var options = setOptions(srcType);
+    var func = createNewFileEntry;
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+        func(imageUri);
+
+    }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+
+    }, options);
+}
+
+    
+    
+}
+
+
+
+
+function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
     }
-
-    if( window.cordova && cordova.version ) {                   // only works in Cordova 3.x
-        app.consoleLog("cordova.version: " + cordova.version) ; // print new Cordova 3.x version string...
-
-        if( cordova.require ) {                                 // print included cordova plugins
-            app.consoleLog(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
-        }
-    }
-
-    app.consoleLog(fName, "exit") ;
-} ;
+    return options;
+}
+    
 
 
 
-// Using a splash screen is optional. This function will not fail if none is present.
-// This is also a simple study in the art of multi-platform device API detection.
 
-app.hideSplashScreen = function() {
-    "use strict" ;
-    var fName = "app.hideSplashScreen():" ;
-    app.consoleLog(fName, "entry") ;
 
-    // see https://github.com/01org/appframework/blob/master/documentation/detail/%24.ui.launch.md
-    // Do the following if you disabled App Framework autolaunch (in index.html, for example)
-    // $.ui.launch() ;
 
-    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
-        navigator.splashscreen.hide() ;
-    }
-    if( window.intel && intel.xdk && intel.xdk.device ) {           // Intel XDK device API detected, but...
-        if( intel.xdk.device.hideSplashScreen )                     // ...hideSplashScreen() is inside the base plugin
-            intel.xdk.device.hideSplashScreen() ;
-    }
 
-    app.consoleLog(fName, "exit") ;
-} ;
+
+
